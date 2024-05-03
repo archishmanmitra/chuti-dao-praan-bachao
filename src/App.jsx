af0953799f6799e5546fb4db818bfbf90f6eb115
Archishman Mitra <riju.archishman@gmail.com>
2024-05-03 15:47:35 +0530

    vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Card from './components/Card'
 import Marquee from './components/Marquee'
 import Form from './components/Form'
 import LocomotiveScroll from 'locomotive-scroll';
+import { Analytics } from "@vercel/analytics/react"
 function App() {
   const locomotiveScroll = new LocomotiveScroll();
   return (
@@ -12,6 +13,7 @@ function App() {
       <Card/>
       <Marquee/>
       <Form/>
+      <Analytics/>
     </>
   )
 }
